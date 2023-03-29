d1ae05881544b0de873dfa6abab874a281ba11b8
MatthewWilliams <turbodieseldodge@gmail.com>
2023-03-29 11:05:07 -0500

    working on seed file

--- server/seed.py
+++ server/seed.py
@@ -1,17 +1,55 @@
 #!/usr/bin/env python3
 
-# Standard library imports
 from random import randint, choice as rc
 
-# Remote library imports
-from faker import Faker
-
-# Local imports
 from app import app
-from models import db
+from models import db, Product, ProductCategory, Color, Order, OrderDetail
+
+# Remote library imports
+# from faker import Faker
 
 if __name__ == '__main__':
-    fake = Faker()
+    # fake = Faker()
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        print('Clearing db...')
+        Product.query.delete()
+        ProductCategory.query.delete()
+        Color.query.delete()
+        Order.query.delete()
+        OrderDetail.query.delete()
+
+        print('Seeding products...')
+        products = [
+            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Smol-pic', category='Item-type'),
+            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Smol-pic', category='Item-type'),
+            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Smol-pic', category='Item-type'),
+        ]
+
+        db.session.add_all(products)
+
+        # print('Seeding product-categories...')
+        # product_categories = [
+        #     ProductCategory(category='Item-type')
+        #     ProductCategory(category='Item-type')
+        #     ProductCategory(category='Item-type')
+        # ]
+
+        # db.session.add_all(product_categories)
+
+        # print('Seeding colors...')
+        # colors = [
+        #     Color(color='Item-color',)
+        #     Color(color='Item-color',)
+        #     Color(color='Item-color',)
+        # ]
+
+        # db.session.add_all(colors)
+
+        print('Seeding orders...')
+        orders = [
+            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date')
+            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date')
+            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date')
+        ]
+
+        db.session.add_all(orders)
