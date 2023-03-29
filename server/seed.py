bdc3614c7f96607a32c524dd5d6ae96ffb1636b6
MatthewWilliams <turbodieseldodge@gmail.com>
2023-03-29 12:28:26 -0500

    add the remaining seed info

--- server/seed.py
+++ server/seed.py
@@ -20,36 +20,46 @@ if __name__ == '__main__':
 
         print('Seeding products...')
         products = [
-            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Smol-pic', category='Item-type'),
-            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Smol-pic', category='Item-type'),
-            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Smol-pic', category='Item-type'),
+            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
+            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
+            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
         ]
 
         db.session.add_all(products)
 
-        # print('Seeding product-categories...')
+        # print('Seeding product_categories...')
         # product_categories = [
-        #     ProductCategory(category='Item-type')
-        #     ProductCategory(category='Item-type')
-        #     ProductCategory(category='Item-type')
+        #     ProductCategory(category='Item-type'),
+        #     ProductCategory(category='Item-type'),
+        #     ProductCategory(category='Item-type'),
         # ]
 
         # db.session.add_all(product_categories)
 
         # print('Seeding colors...')
         # colors = [
-        #     Color(color='Item-color',)
-        #     Color(color='Item-color',)
-        #     Color(color='Item-color',)
+        #     Color(color='Item-color',),
+        #     Color(color='Item-color',),
+        #     Color(color='Item-color',),
         # ]
 
         # db.session.add_all(colors)
 
+        # print('Seeding order_details')
+        # order_details = [
+        #     OrderDetail(price='$', sku='12345', quantity='1'),
+        #     OrderDetail(price='$', sku='12345', quantity='1'),
+        #     OrderDetail(price='$', sku='12345', quantity='1'),
+        # ]
+
+        # db.session.add_all(order_details)
+
         print('Seeding orders...')
         orders = [
-            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date')
-            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date')
-            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date')
+            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
+            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
+            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
         ]
 
         db.session.add_all(orders)
+
