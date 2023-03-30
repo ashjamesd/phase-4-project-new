b6b26185b68838e62550053e01d0d075a8576cc2
Ash <ashjames.dean@gmail.com>
2023-03-30 01:01:54 -0400

    got data to fetch from db instead of hardcode

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,47 @@
-import React from 'react';
+// import React from 'react';
 
-const ProductList = ({ products, onAddToCart }) => {
+// const ProductList = ({ products, onAddToCart }) => {
+//   return (
+//     <div>
+//       <h2>Featured Products</h2>
+//       {products.map((product) => (
+//         <div key={product.id}>
+//           <img src={product.imageUrl} alt={product.name} />
+//           <h3>{product.name}</h3>
+//           <p>{product.price}</p>
+//           <button onClick ={()=>onAddToCart(product)}>Add to Cart</button>
+
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default ProductList;
+
+import React, { useState, useEffect } from 'react';
+
+const ProductList = ({ onAddToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch('http://localhost:3000/products');
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h2>Featured Products</h2>
       {products.map((product) => (
         <div key={product.id}>
-          <img src={product.imageUrl} alt={product.name} />
+          <img className='productImage' src={product.image} alt={product.name} />
           <h3>{product.name}</h3>
           <p>{product.price}</p>
-          <button onClick ={()=>onAddToCart(product)}>Add to Cart</button>
-
+          <button onClick={() => onAddToCart(product)}>Add to cart</button>
         </div>
       ))}
     </div>
