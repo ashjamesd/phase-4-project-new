81f1a96de71c9c80b9f8854b4ed2ba257bcf5fe7
Ash <ashjames.dean@gmail.com>
2023-03-30 02:43:22 -0400

    trying some new things with Seed and Models

--- server/models.py
+++ server/models.py
@@ -1,62 +1,118 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy_serializer import SerializerMixin
+
+# from config import db
+
+# db = SQLAlchemy()
+
+# class Product(db.Model, SerializerMixin):
+#     __tablename__ = 'products'
+
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String)
+#     price = db.Column(db.Float)
+#     image = db.Column(db.String)
+#     # weight = db.Column(db.Float)
+#     # description = db.Column(db.Integer)
+#     # thumbnail = db.Column(db.String)
+#     category = db.Column(db.String)
+#     created_at = db.Column(db.DateTime, server_default = db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+#     # in_stock = db.Column(db.Boolean)
+
+#     product_categories = db.relationship('ProductCategory', backref='product')
+#     color = db.relationship('ProductCategory', backref='color')
+
+# class ProductCategory(db.Model, SerializerMixin):
+#     __tablename__ = 'product_categories'
+
+#     id = db.Column(db.Integer, primary_key = True)
+#     product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+#     name = db.Column(db.String)
+
+
+# class Color(db.Model, SerializerMixin):
+#     __tablename__ = 'colors'
+
+#     id = db.Column(db.Integer, primary_key = True)
+#     product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+#     color = db.Column(db.String)
+
+# class OrderDetail(db.Model, SerializerMixin):
+#     __tablename__ = 'order_details'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     order_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+#     product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+#     price = db.Column(db.Float)
+#     sku = db.Column(db.Integer)
+#     quantity = db.Column(db.Integer)
+
+# class Order(db.Model, SerializerMixin):
+#     __tablename__ = 'orders'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     customer_name = db.Column(db.String)
+#     email = db.Column(db.String)
+#     password = db.Column(db.String)
+#     shipping_address = db.Column(db.String)
+#     billing_address = db.Column(db.String)
+#     order_email = db.Column(db.String)
+#     order_date = db.Column(db.DateTime, server_default=db.func.now())
+#     order_status = db.Column(db.String)
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-from config import db
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=metadata)
 
-class Product(db.Model, SerializerMixin):
-    __tablename__ = 'products'
+# Add models here
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    serialize_rules = ('-restaurants.restaurant_pizzas', '-restaurant_pizzas.pizza')
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
-    price = db.Column(db.Float)
-    images = db.Column(db.String)
-    weight = db.Column(db.Float)
-    description = db.Column(db.Integer)
-    thumbnail = db.Column(db.String)
-    category = db.Column(db.String)
+    ingredients = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default = db.func.now())
     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
-    in_stock = db.Column(db.Boolean)
 
-    product_categories = db.relationship('ProductCategory', backref='product')
-    color = db.relationship('ProductCategory', backref='color')
+    pizza_restaurants = db.relationship('RestaurantPizza', backref = 'pizza')
 
-class ProductCategory(db.Model, SerializerMixin):
-    __tablename__ = 'product_categories'
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    serialize_rules = ('-pizzas.pizza_restaurants', '-restaurant_pizzas.restaurant')
 
     id = db.Column(db.Integer, primary_key = True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
     name = db.Column(db.String)
+    address = db.Column(db.String)
 
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'restaurant')
 
-class Color(db.Model, SerializerMixin):
-    __tablename__ = 'colors'
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    serialize_rules = ('-pizza.pizza_restaurants', '-restaurant.restaurant_pizzas')
 
     id = db.Column(db.Integer, primary_key = True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    color = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
 
-class OrderDetail(db.Model, SerializerMixin):
-    __tablename__ = 'order_details'
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
-    id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    price = db.Column(db.Float)
-    sku = db.Column(db.Integer)
-    quantity = db.Column(db.Integer)
-
-class Order(db.Model, SerializerMixin):
-    __tablename__ = 'orders'
-
-    id = db.Column(db.Integer, primary_key=True)
-    customer_name = db.Column(db.String)
-    email = db.Column(db.String)
-    password = db.Column(db.String)
-    shipping_address = db.Column(db.String)
-    billing_address = db.Column(db.String)
-    order_email = db.Column(db.String)
-    order_date = db.Column(db.DateTime, server_default=db.func.now())
-    order_status = db.Column(db.String)
+    @validates('price')
+    def validates_price(self, key, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be betweeen 1 and 30, exclusive!")
+        return value
--- server/seed.py
+++ server/seed.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
-from random import randint, choice as rc
+# from random import randint, choice as rc
 
-from app import app
-from models import db, Product, ProductCategory, Color, Order, OrderDetail
+# from app import app
+# from models import db, Product, ProductCategory, Color, Order, OrderDetail
 
 # Remote library imports
 # from faker import Faker
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # fake = Faker()
-    with app.app_context():
-        print('Clearing db...')
-        Product.query.delete()
-        ProductCategory.query.delete()
-        Color.query.delete()
-        Order.query.delete()
-        OrderDetail.query.delete()
+    # with app.app_context():
+    #     print('Clearing db...')
+    #     Product.query.delete()
+    #     ProductCategory.query.delete()
+    #     Color.query.delete()
+    #     Order.query.delete()
+    #     OrderDetail.query.delete()
 
-        print('Seeding products...')
-        products = [
-            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
-            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
-            Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
-        ]
+    #     print('Seeding products...')
+    #     products = [
+    #         Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
+    #         Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
+    #         Product(name='Item', sku='12345', price='$', images='Pix', weight='Lbs', description='Details', thumbnail='Small-pic', category='Item-type'),
+    #     ]
 
-        db.session.add_all(products)
+    #     db.session.add_all(products)
 
         # print('Seeding product_categories...')
         # product_categories = [
@@ -54,12 +54,56 @@ if __name__ == '__main__':
 
         # db.session.add_all(order_details)
 
-        print('Seeding orders...')
-        orders = [
-            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
-            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
-            Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
-        ]
+        # print('Seeding orders...')
+        # orders = [
+        #     Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
+        #     Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
+        #     Order(customer='Customer-name', email='Email', password='PW', shipping_address='Shipping-address', billing_address='Billing-Address', order_email='Order-email', order_date='Order-date'),
+        # ]
 
-        db.session.add_all(orders)
+        # db.session.add_all(orders)
 
+
+#!/usr/bin/env python3
+from random import choice as rc
+# from faker import Faker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app import app
+from models import db, Restaurant, Pizza, RestaurantPizza
+
+with app.app_context():
+
+# This will delete any existing rows
+# so you can run the seed file multiple times without having duplicate entries in your database
+    print("Deleting data...")
+    Pizza.query.delete()
+    Restaurant.query.delete()
+    RestaurantPizza.query.delete()
+
+    print("Creating restaurants...")
+    shack = Restaurant(name = "Karen's Pizza Shack", address = 'address1')
+    bistro = Restaurant(name = "Sanjay's Pizza", address = 'address2')
+    palace = Restaurant(name = "Kiki's Pizza", address = 'address3')
+    restaurants = [shack, bistro, palace]
+
+    print("Creating pizzas...")
+
+
+    cheese = Pizza(name = "Emma", ingredients = "Dough, Tomato Sauce, Cheese")
+    pepperoni = Pizza(name = "Geri", ingredients = "Dough, Tomato Sauce, Cheese, Pepperoni")
+    california = Pizza(name = "Melanie", ingredients = "Dough, Sauce, Ricotta, Red peppers, Mustard")
+    pizzas = [cheese, pepperoni, california]
+
+    print("Creating RestaurantPizza...")
+
+    pr1 = RestaurantPizza(restaurant = shack, pizza = cheese, price = 1)
+    pr2 = RestaurantPizza(restaurant = bistro, pizza  = pepperoni, price = 4)
+    pr3 = RestaurantPizza(restaurant = palace, pizza = california, price = 5)
+    restaurantPizzas = [pr1, pr2, pr3]
+    db.session.add_all(restaurants)
+    db.session.add_all(pizzas)
+    db.session.add_all(restaurantPizzas)
+    db.session.commit()
+
+    print("Seeding done!")
